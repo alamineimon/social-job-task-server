6ad70b169c2752a011db7338939e7586180fa015
Al Amin Eimon <eimonalamin@gmail.com>
2023-03-13 01:57:51 +0600

    posted api creatted

--- index.js
+++ index.js
@@ -29,7 +29,13 @@ async function run() {
       .collection("users");
  
 
-    //featured e sports games
+    //post all post
+    app.post('/allpost', async (req, res) => {
+      const users = req.body;
+      const result = await allPostCollection.insertOne(users);
+      res.send(result);
+  })
+    //get all post
     app.get("/allpost", async (req, res) => {
       const query = {};
       const posts = await allPostCollection.find(query).toArray();
@@ -47,6 +53,36 @@ async function run() {
       const state = await allUsersCollection.find(query).toArray();
       res.send(state);
   })
+
+      // user Profile Update
+      app.get("/profileUpdate/:email", async (req, res) => {
+        const email = req.params.email;
+        const query = { email: email };
+        const user = await allUsersCollection.findOne(query);
+        res.send(user);
+      });
+  
+      app.patch("/profileUpdate/:id", async (req, res) => {
+        const id = req.params.id;
+        const profile = req.body;
+        const query = { _id: ObjectId(id) };
+        const option = { upsert: true };
+        const updateDoc = {
+          $set: {
+            name: profile.name,
+            email: profile.email,
+            photoURL: profile.photoURL,
+            facebook: profile.facebook,
+            instagram: profile.instagram,
+            youTube: profile.youTube,
+            twitter: profile.twitter,
+            bio: profile.bio,
+          },
+        };
+        const result = await allUsersCollection.updateOne(query, updateDoc, option);
+        res.send(result);
+      });
+  
 
 
 
